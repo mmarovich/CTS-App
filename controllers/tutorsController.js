3a9d0e2792be9e23f2d7b51b2cfd95dcdb50d236
mmarovich <mmarovich@gmail.com>
2019-11-02 19:31:50 +0800

    pulling tutors from api to dash

--- controllers/tutorsController.js
+++ controllers/tutorsController.js
@@ -2,13 +2,12 @@ const db = require("../models");
 
 module.exports = {
   findAll: function(req, res) {
-    console.log("here")
     db.User.find({}, (err, tutors) => {
       if (err) {
         console.log(err)
       }
 
-      res.status(400).json({tutors})
+      res.json({tutors})
     })
   },
 }
