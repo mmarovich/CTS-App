363b8eb55503d38e2e31a6f62dcb0f57ba123598
mmarovich <mmarovich@gmail.com>
2019-11-05 17:48:13 +0800

    added tabs that re-mount component

--- client/src/components/admin/Admin.js
+++ client/src/components/admin/Admin.js
@@ -1,119 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { Main, OnHold, Active, Inactive, Resigned } from '../admin/tabs'
 
-import {Main, OnHold, Active, Inactive, Resigned} from "./tabs"
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
+const Admin = () => {
   return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
+    <Tabs>
+      <TabList>
+        <Tab>Main</Tab>
+        <Tab>On Hold</Tab>
+        <Tab>Active</Tab>
+        <Tab>Inactive</Tab>
+        <Tab>Resigned</Tab>
+      </TabList>
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
-}
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={event => {
-        event.preventDefault();
-      }}
-      {...props}
-    />
-  );
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
-export default function Admin() {
-  const classes = useStyles();
-  const [value, setValue] = useState(0);
-
-  const [tutors, setTutors] = useState();
-
-  useEffect(() => {
-    updateTutors()
-  }, [])
-
-  const updateTutors = () => {
-    axios.get("/api/admin/tutors").then(allTutors => {
-      setTutors(allTutors.data.tutors)
-    }).catch(err => {
-      console.log(err)
-    })
-    console.log("Tutor List Updated!");
-}
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs
-          variant="fullWidth"
-          value={value}
-          onChange={handleChange}
-          aria-label="nav tabs example"
-        >
-          <LinkTab label="Main" href="/drafts" {...a11yProps(0)} />
-          <LinkTab label="On Hold" href="/trash" {...a11yProps(1)} />
-          <LinkTab label="Active" href="/spam" {...a11yProps(2)} />
-          <LinkTab label="Inactive" href="/spam" {...a11yProps(2)} />
-          <LinkTab label="Resigned" href="/spam" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
+      <TabPanel>
         <Main />
       </TabPanel>
-      <TabPanel value={value} index={1}>
-        <OnHold updateTutors={updateTutors} />
+      <TabPanel>
+        <OnHold />
       </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Active tutors={tutors} updateTutors={updateTutors} />
+      <TabPanel>
+        <Active />
       </TabPanel>
-      <TabPanel value={value} index={3}>
-        <Inactive tutors={tutors} updateTutors={updateTutors} />
+      <TabPanel>
+        <Inactive />
       </TabPanel>
-      <TabPanel value={value} index={4}>
-        <Resigned tutors={tutors} updateTutors={updateTutors} />
+      <TabPanel>
+        <Resigned />
       </TabPanel>
-    </div>
-  );
+    </Tabs>
+  )
 }
+
+export default Admin;
--- client/src/components/admin/tabs/Main.js
+++ client/src/components/admin/tabs/Main.js
@@ -14,6 +14,7 @@ const Main = (props) => {
   }
 
   useEffect(() => {
+    console.log("mounted")
     getAllTutors();
   }, [])
 
